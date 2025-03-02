f9b490b2ed855ed723687cc564c907a4cafa16cb
Artur Wojnar <artur@orikami.nl>
2025-03-02 20:29:07 +0100

    Added missing @nuxt/ui-templates

--- tokens.config.ts
+++ tokens.config.ts
@@ -1,16 +1,2 @@
 import { defineTheme } from "pinceau";
-export default defineTheme({
-  // alpine: {
-  //   hero: {
-  //     image: {
-  //       light: "/logo.svg",
-  //       dark: "/logo-dark.svg",
-  //     },
-  //   },
-  // },
-  // color: {
-  //   white: "#FFFFFF",
-  //   // @ts-ignore
-  //   // primary: "{theme.color.lightblue}",
-  // },
-});
+export default defineTheme({});
