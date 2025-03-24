bceeb4501f0ce88e0411f5dc2439580a96d185d0
Artur Wojnar <artur@orikami.nl>
2025-03-24 21:56:07 +0100

    Likes functionality

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,13 +4,25 @@ export default defineNuxtConfig({
   css: ["~/assets/css/main.css"],
   modules: [
     // https://github.com/nuxt-modules/plausible
-    "@nuxtjs/plausible",
-    // https://github.com/nuxt/devtools
+    "@nuxtjs/plausible", // https://github.com/nuxt/devtools
     "@nuxt/devtools",
+    "@nuxtjs/supabase",
   ],
 
   compatibilityDate: "2025-03-01",
 
+  supabase: {
+    // Set redirect to false directly
+    redirect: false,
+    // Important for Vercel deployment
+    clientOptions: {
+      auth: {
+        persistSession: false,
+        autoRefreshToken: false,
+        detectSessionInUrl: false,
+      },
+    },
+  },
   runtimeConfig: {
     FORMSPREE_URL: process.env.FORMSPREE_URL,
   },
